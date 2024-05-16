e54d66df1d13be22c1abfae524870cd1817761ed
lokiagg <1306426768@qq.com>
2024-05-16 10:01:49 +0800

    [back]

--- ycsb/generate_full_workloads.sh
+++ ycsb/generate_full_workloads.sh
@@ -2,10 +2,10 @@
 
 start_time=$(date +%s)
 
-for WORKLOAD_TYPE in la a b c d f g h i ; do
-#for WORKLOAD_TYPE in a ; do
+#for WORKLOAD_TYPE in la a b c d; do
+for WORKLOAD_TYPE in a ; do
   python3 gen_workload.py workload${WORKLOAD_TYPE} randint full
-#  python3 gen_workload.py workload${WORKLOAD_TYPE} email full
+  python3 gen_workload.py workload${WORKLOAD_TYPE} email full
 done
 
 #for WORKLOAD_TYPE in e; do
