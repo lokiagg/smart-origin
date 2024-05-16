b0c202bde19672c4f16a689085111bc25932f422
lokiagg <1306426768@qq.com>
2024-05-16 11:24:26 +0800

    [workloads]

--- ycsb/generate_full_workloads.sh
+++ ycsb/generate_full_workloads.sh
@@ -2,18 +2,18 @@
 
 start_time=$(date +%s)
 
-for WORKLOAD_TYPE in la a b c d; do
+for WORKLOAD_TYPE in la a b c d f g h i; do
   python3 gen_workload.py workload${WORKLOAD_TYPE} randint full
-  python3 gen_workload.py workload${WORKLOAD_TYPE} email full
+#  python3 gen_workload.py workload${WORKLOAD_TYPE} email full
 done
 
-for WORKLOAD_TYPE in e; do
-  python3 gen_workload.py workload${WORKLOAD_TYPE} randint full
-done
+#for WORKLOAD_TYPE in e; do
+#  python3 gen_workload.py workload${WORKLOAD_TYPE} randint full
+#done
 
-for WORKLOAD_TYPE in 0- 10- 20- 30- 40- 50- 60- 70- 80- 90- 100-; do
-  python3 gen_workload.py workload${WORKLOAD_TYPE} email full
-done
+#for WORKLOAD_TYPE in 0- 10- 20- 30- 40- 50- 60- 70- 80- 90- 100-; do
+#  python3 gen_workload.py workload${WORKLOAD_TYPE} email full
+#done
 
 end_time=$(date +%s)
 cost_time=$(($end_time-$start_time))
