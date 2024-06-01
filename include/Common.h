b9307bf541be081eb1e60e8cdcded18223bba1cb
lokiagg <1306426768@qq.com>
2024-06-01 14:19:23 +0800

    [debug]

--- include/Common.h
+++ include/Common.h
@@ -20,7 +20,7 @@
 // Environment Config
 #define MAX_MACHINE 20
 #define MEMORY_NODE_NUM 2
-#define CPU_PHYSICAL_CORE_NUM 64  // [CONFIG]
+#define CPU_PHYSICAL_CORE_NUM 62  // [CONFIG]
 #define MAX_CORO_NUM 8
 
 #define LATENCY_WINDOWS 100000
